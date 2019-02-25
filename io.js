27a36bf0d0705b6d66530477420f99d9d4cb46c1
unknown <kwaihung1314@hotmail.com>
2019-02-25 17:48:24 +0800

    express structure

---
+++ io.js
@@ -0,0 +1,47 @@
+module.exports = (io) => {
+    let onlineUsers = {};
+
+    io.on('connection', (socket) => {
+        console.log('connected: ', socket.id);
+        io.to(socket.id).emit('online user', onlineUsers);
+        socket.on('user entered', (data) => {
+            console.log(data);
+            onlineUsers[socket.id] = data;
+            io.emit('update users', {
+                name: data,
+                id: socket.id
+            }, `${data} have entered`, 'enter');
+        });
+        socket.on('disconnect', () => {
+            console.log('closed: ', socket.id);
+            let userLeft = onlineUsers[socket.id];
+            delete onlineUsers[socket.id];
+            io.emit('update users', {
+                name: userLeft,
+                id: socket.id
+            }, `${userLeft} has left`, 'left');
+            console.log(onlineUsers);
+        });
+        socket.on('group message', (data) => {
+            console.log(data);
+            io.emit('new group message', data);
+        });
+        socket.on('typing', (user) => {
+            io.emit('whos typing', user);
+        })
+        socket.on('group image', (data, file) => {
+            console.log(file)
+            io.emit('new group image', data, file);
+        })
+        socket.on('private message', (data) => {
+            console.log('private')
+            io.to(data.toId).emit('new private message', data)
+            io.to(data.fromId).emit('my private message', data)
+        })
+        socket.on('private image', (data, file) => {
+            console.log('private')
+            io.to(data.toId).emit('new private image', data, file)
+            io.to(data.fromId).emit('my private image', data, file)
+        })
+    });
+}
